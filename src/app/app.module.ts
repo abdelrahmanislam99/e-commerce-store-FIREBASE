8cb66599113195960b3457ade41066ff59cdb928
abdelrahmanislam99 <88116183+abdelrahmanislam99@users.noreply.github.com>
2023-05-20 13:01:38 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,7 +54,13 @@ import { GoodsComponent } from './Components/goods/goods.component';
     MatButtonModule,
     FormsModule,
     AngularFireModule.initializeApp({
-      //  firebase initialize object here
+      apiKey: "AIzaSyAl3FYrOt2PKe1kPJUdQ4otzBikI8CZJNQ",
+      authDomain: "e-commerce-store-99252.firebaseapp.com",
+      projectId: "e-commerce-store-99252",
+      storageBucket: "e-commerce-store-99252.appspot.com",
+      messagingSenderId: "622318229668",
+      appId: "1:622318229668:web:cc9274117c8edfe1b06e31",
+      measurementId: "G-B7L49XWKQW"
     }),
     AngularFirestoreModule,
     AngularFireAuthModule,
