8769c29c539c29d2cb5c4d6cb905252f85cf7724
abdelrahmanislam99 <88116183+abdelrahmanislam99@users.noreply.github.com>
2023-05-20 19:01:29 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { SignUpComponent } from './Components/sign-up/sign-up.component';
 import { HomeComponent } from './Components/home/home.component';
 import { CartComponent } from './Components/cart/cart.component';
 import { GoodsComponent } from './Components/goods/goods.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -53,15 +54,7 @@ import { GoodsComponent } from './Components/goods/goods.component';
     HttpClientModule,
     MatButtonModule,
     FormsModule,
-    AngularFireModule.initializeApp({
-      apiKey: "AIzaSyAl3FYrOt2PKe1kPJUdQ4otzBikI8CZJNQ",
-      authDomain: "e-commerce-store-99252.firebaseapp.com",
-      projectId: "e-commerce-store-99252",
-      storageBucket: "e-commerce-store-99252.appspot.com",
-      messagingSenderId: "622318229668",
-      appId: "1:622318229668:web:cc9274117c8edfe1b06e31",
-      measurementId: "G-B7L49XWKQW"
-    }),
+    AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireStorageModule,
